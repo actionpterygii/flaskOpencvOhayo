ee8cae47fdb803efdd2da71dd20c7072fd881004
actionpterygii <actionpterygii@gmail.com>
2018-11-11 13:24:51 +0900

    konnichiwa

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, render_template
+from datetime import datetime
+import numpy as np
+import cv2
+import os
+
+app = Flask(__name__)
+
+SAVE_DIR = "./static"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/", methods = ["POST"])
+def upload():
+
+    stream = request.files['image'].stream
+    img_array = np.asarray(bytearray(stream.read()), dtype=np.uint8)
+    im = cv2.imdecode(img_array, 1)
+
+    cascade = cv2.CascadeClassifier('cascade.xml')
+    faces = cascade.detectMultiScale(im, 1.1, 3) 
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(im, (x,y), (x+w,y+h), (255, 0, 0), thickness=6) 
+    
+    dt_now = datetime.now().strftime("%Y_%m_%d%_H_%M_%S_")
+    save_path = os.path.join(SAVE_DIR, dt_now + ".png")
+    cv2.imwrite(save_path, im)
+
+    return render_template('index.html', im=os.listdir(SAVE_DIR)[-1])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
